a6440d3ec11c814130502b4a06106dfbe7779b58
faisal4847 <47690553+faisal4847@users.noreply.github.com>
2019-06-13 10:19:43 +0500

    after refectorng method

--- src/labTerminalB/Rental.java
+++ src/labTerminalB/Rental.java
@@ -13,4 +13,25 @@ public class Rental {
 	public Movie getMovie() {
 		return _movie;
 	}
+	
+	double movie() {
+		double Totalount = 0;
+		switch (getMovie().getPriceCode()) {
+	
+		case Movie.REGULAR:
+			Totalount += 2;
+			if (getDaysRented() > 2)
+				Totalount += (getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			Totalount += getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			Totalount += 1.5;
+			if (getDaysRented() > 3)
+				Totalount += (getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return Totalount;
+	}
 }
